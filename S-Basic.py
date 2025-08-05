a596f1a88835a3e42c9dcbd39bb946eb20c3e5b2
sohantanvir <tanvirsohan316@gmail.com>
2025-08-05 20:53:10 +0600

    Add files via upload

---
+++ S-Basic.py
@@ -0,0 +1,66 @@
+# Basic package install
+import os
+import sys
+import time
+
+
+os.system("clear")
+#os.system('pwd')
+print("\n\n\n")
+
+ab="    \033[1;32m  Wellcome to hacking world..........."
+for c in ab:
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.1)
+print("\n\n")
+	
+ab="      \033[1;34mSuccesfully Loaded...........!"
+for c in ab:
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.1)	
+print("\n\n")
+name = input (" Your Name :")
+print("\n\n")
+ab="      \033[1;33mHey "+name+" Be ethical......!"
+for c in ab:
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.1)
+print("\n\n\n")
+time.sleep(2)
+os.system("clear")
+print("""\033[1;36m
+
+
+  _           _                _       _   _     
+ | |         | |              (_)     | | | |    
+ | |     __ _| |__  _   _ _ __ _ _ __ | |_| |__  
+ | |    / _` | '_ \\| | | | '__| | '_ \\| __| '_ \\ 
+ | |___| (_| | |_) | |_| | |  | | | | | |_| | | |
+ |______\\__,_|_.__/ \\__, |_|  |_|_| |_|\\__|_| |_|
+                     __/ |                       
+                    |___/                        
+
+\033[1;32m============================================================
+\033[1;34mOwner : Sohan Tanvir
+
+Teligram :@Psycotanvir
+
+Facebook: https://www.facebook.com/SOHAN.TANVIR.LABYRINTH
+
+Github: Something..
+
+============================================================
+	
+""")
+#import os
+print("\033[1;32m")
+os.system("pkg up -y")
+#print(\033)
+os.system("pkg install git -y")
+os.system("pkg install ruby -y")
+os.system("gem install lolcat")
+os.system("pip install requests")
+print(" All Package installed successfuly.!")
